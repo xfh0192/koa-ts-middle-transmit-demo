ab9fe1d43d570843fcd10627bc261c2d5fc36462
xfh0192 <461144823@qq.com>
2018-05-23 13:13:45 +0800

    脚本命令。。nodemon的--exec正确用法。。

--- src/controller/mobile-special-view.ts
+++ src/controller/mobile-special-view.ts
@@ -3,7 +3,7 @@ import {NextFunction} from 'connect'
 
 let specialRes = async (ctx: Context, next: NextFunction) => {
     ctx.status = 200;
-    ctx.body = 'gg';
+    ctx.body = '222';
     next();
 }
 
